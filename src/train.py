dda9a8db0a702cfb3e98b1224a77ca051e9d901d
lamduong33 <lamthachduong@gmail.com>
2020-04-24 16:40:43 -0400

    Tiny change to comment

--- src/train.py
+++ src/train.py
@@ -18,6 +18,7 @@ if gpus:
 
 def DataSet():
     def __init__(self, t_train_data_dir, t_test_data_dir):
+        """ Constructor for the image data """
         self.test_data_dir = t_test_data_dir
         self.train_data_dir = t_train_data_dir
 
