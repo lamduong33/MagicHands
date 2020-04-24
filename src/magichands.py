9c5f39cf2ebe79cccd658f4c829d12aca83cc5d8
lamduong33 <lamthachduong@gmail.com>
2020-04-24 19:53:17 -0400

    Testing image data

--- src/magichands.py
+++ src/magichands.py
@@ -19,23 +19,29 @@ class DataSet:
         data that might be used for evaluation purposes and further classification. """
     def __init__(self, t_train_data_dir, t_test_data_dir):
         """ Constructor for the image data """
+        self.image_height, self.image_width = 200, 200
+        self.batch_size = 64
         self.test_data_dir = t_test_data_dir
         self.train_data_dir = t_train_data_dir
         self.train_data_generator = keras.preprocessing.image.ImageDataGenerator(rotation_range=40,
-                width_shift_range=0.2, height_shift_range=0.2, rescale=1./255, shear_range=0.2, 
+                width_shift_range=0.2, height_shift_range=0.2, rescale=1./255, shear_range=0.2,
                 zoom_range=0.2, horizontal_flip=True, fill_mode="nearest")
         self.test_data_generator = keras.preprocessing.image.ImageDataGenerator(rotation_range=40,
-                width_shift_range=0.2, height_shift_range=0.2, rescale=1./255, shear_range=0.2, 
+                width_shift_range=0.2, height_shift_range=0.2, rescale=1./255, shear_range=0.2,
                 zoom_range=0.2, horizontal_flip=True, fill_mode="nearest")
-        self.training_data = self.train_data_generator.flow_from_directory(self.train_data_dir)
-        self.testing_data = self.test_data_generator.flow_from_directory(self.test_data_dir)
+        self.training_data = self.train_data_generator.flow_from_directory(self.train_data_dir, target_size=(self.image_height, self.image_width), batch_size=self.batch_size, class_mode="binary")
+        self.testing_data = self.test_data_generator.flow_from_directory(self.test_data_dir, target_size=(self.image_height, self.image_width), batch_size=self.batch_size, class_mode="binary")
 
 class ConvNet:
     """ The convolutional neural net model """
-    def __init__(self):
+    def __init__(self, t_dataset):
         """ Construct the layers """
-        print("HELLO WORLD")
+        self.model = keras.models.Sequential()
+        self.data = t_dataset
+
 
 def main():
-    data = DataSet("data/train", "data_test")
-
+    data = DataSet("data/train", "data/test")
+    cnn = ConvNet(data)
+    cnn.model.fit_generator(cnn.data.trcnn_modelcnn_modelain_data_generator, steps_per_epoch=2000, epochs=50, validation_data=cnn.data.test_data_generator, validation_steps=800)
+    print("Done!")
