ba66ea61477e6fb9a375d1d7d8c3615128b61972
lamduong33 <lamthachduong@gmail.com>
2020-04-24 03:51:56 -0400

    Organized new data

--- src/train.py
+++ src/train.py
@@ -1,8 +1,15 @@
 # Author: Lam Duong
 
+import numpy
+import keras
+import sklearn
+import scipy
+
 def DataSet():
     def __init__(self, t_file_name):
         self.file_name = t_file_name;
 
 def main():
     print("Training data")
+
+main()
