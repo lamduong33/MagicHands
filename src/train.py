d3c1ee2da7f932fd2ec42268b3b3d13bd5a76113
lamduong33 <lamthachduong@gmail.com>
2020-04-24 15:30:52 -0400

    Finally got TensorFlow and CUDA to work with GPU

--- src/train.py
+++ src/train.py
@@ -10,28 +10,55 @@ def DataSet():
         self.file_name = t_file_name
 
 def main():
-    data_generator = keras.preprocessing.image.ImageDataGenerator(rotation_range=40,
+    train_data_generator = keras.preprocessing.image.ImageDataGenerator(rotation_range=40,
                                                                   width_shift_range=0.2,
                                                                   height_shift_range=0.2,
-                                                                  rescale=1/255,
+                                                                  rescale=1./255,
                                                                   shear_range=0.2,
                                                                   zoom_range=0.2,
                                                                   horizontal_flip=True,
-                                                                  fill_mode='nearest')
+                                                                  fill_mode="nearest")
+
+    batch_size = 16
+
+    # TESTING DATA IN BATCH
+    test_data_generator = keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
 
     # TRAINING DATA IN BATCH
-    training_data = data_generator.flow_from_directory('data/train', class_mode='binary', batch_size=64)
+    training_data = train_data_generator.flow_from_directory('data/train', target_size=(200,200), class_mode='binary', batch_size=batch_size)
+
+    # VALIDATION DATA
+    validation_generator = test_data_generator.flow_from_directory("data/validation", target_size=(200,200), batch_size=batch_size, class_mode="binary")
 
     # Single data test
     image = keras.preprocessing.image.load_img("data/train/A/A158.jpg")
 
-    x = keras.preprocessing.image.img_to_array(image)
-    x = x.reshape((1,) + x.shape)
+    # Training model with 3 layers for height, width, and features
+    model = keras.models.Sequential()
+    model.add(keras.layers.Conv2D(32, (3, 3), input_shape=(3, 200, 200)))
+    model.add(keras.layers.Activation("relu"))
+    model.add(keras.layers.MaxPooling2D(pool_size=(2, 2), dim_ordering="th"))
 
-    i = 0
-    for batch in data_generator.flow(x, batch_size=1, save_to_dir="preview", save_prefix="A", save_format="jpeg"):
-        i += 1
-        if i > 20:
-            break
+    model.add(keras.layers.Conv2D(32, (3, 3)))
+    model.add(keras.layers.Activation("relu"))
+    model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(keras.layers.Conv2D(32, (3, 3)))
+    model.add(keras.layers.Activation("relu"))
+    model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dense(64))
+    model.add(keras.layers.Activation("relu"))
+    model.add(keras.layers.Dropout(0.5))
+    model.add(keras.layers.Dense(1))
+    model.add(keras.layers.Activation("sigmoid"))
+
+    model.compile(loss="binary_crossentropy",
+                  optimizer="rmsprop",
+                  metric=["accuracy"])
+    
+    model.fit_generator(train_data_generator, steps_per_epoch=2000 // batch_size, epochs=50, validation_data=validation_generator, validation_steps=800//batch_size)
+    model.save_weights("first_try.h5")
 
 main()
