4bd14a57f0f8b29319cca9b2fe18fb24a242af6c
Mustafa Hussain <mustafahamza5@gmail.com>
2020-04-24 18:13:01 -0400

    Started train.py file

--- src/train.py
+++ src/train.py
@@ -1,20 +1,7 @@
-# Author: Lam Duong
 
 import numpy
 import keras
 import tensorflow
-
-""" Ensuring that GPUs are used """
-gpus = tensorflow.config.experimental.list_physical_devices('GPU')
-if gpus:
-  # Restrict TensorFlow to only use the first GPU
-  try:
-    tensorflow.config.experimental.set_visible_devices(gpus[0], 'GPU')
-    logical_gpus = tensorflow.config.experimental.list_logical_devices('GPU')
-    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
-  except RuntimeError as e:
-    # Visible devices must be set before GPUs have been initialized
-    print(e)
 
 def DataSet():
     def __init__(self, t_train_data_dir, t_test_data_dir):
@@ -23,13 +10,8 @@ def DataSet():
         self.train_data_dir = t_train_data_dir
 
 def main():
-    train_data_generator = keras.preprocessing.image.ImageDataGenerator(rotation_range=40,
-                                                                  width_shift_range=0.2,
-                                                                  height_shift_range=0.2,
-                                                                  rescale=1./255,
-                                                                  shear_range=0.2,
-                                                                  zoom_range=0.2,
-                                                                  horizontal_flip=True,
-                                                                  fill_mode="nearest")
-
+    return
 main()
+
+if __name__ == "__main__":
+    main()
