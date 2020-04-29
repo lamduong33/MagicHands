7c420ebf9457bac76c4b381a620bf68ba4fc67b8
lamduong33 <lamthachduong@gmail.com>
2020-04-28 23:09:42 -0400

    ConvNet functional

--- src/magichands.py
+++ src/magichands.py
@@ -1,9 +1,11 @@
-import cv2
-import os
+import cv2 # for dealing with images - OpenCV 
+import os # For dealing with directories
+import gc # For garbage collection
 import numpy # For dealing with arrays
 import sklearn # For dealing with data wrangling
 import keras # For convnet
 import tensorflow # Keras's backbone
+import random # For shuffling
 
 """ Ensuring that GPU is used """
 GPUS = tensorflow.config.experimental.list_physical_devices('GPU')
@@ -22,6 +24,7 @@ class DataSet:
         data that might be used for evaluation purposes and further classification. """
     def __init__(self, t_train_data_dir, t_test_data_dir, t_batch_size):
         """ Constructor for the image data """
+
         self.classes = ['A', 'B', 'C', 'D', "del", 'E', 'F', 'G', 'H', 'I', 'J']
         self.classes = self.classes + ['K', 'L', 'M', 'N', "nothing", 'O', 'P', 'Q', 'R']
         self.classes = self.classes + ['S', "space", 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -30,6 +33,8 @@ class DataSet:
         self.batch_size = t_batch_size
         self.test_data_dir = t_test_data_dir
         self.train_data_dir = t_train_data_dir
+
+        """
         self.train_data_generator = keras.preprocessing.image.ImageDataGenerator(rotation_range=40,
                 width_shift_range=0.2, height_shift_range=0.2, rescale=1./255, shear_range=0.2,
                 zoom_range=0.2, horizontal_flip=True, fill_mode="nearest")
@@ -37,12 +42,25 @@ class DataSet:
                 width_shift_range=0.2, height_shift_range=0.2, rescale=1./255, shear_range=0.2,
                 zoom_range=0.2, horizontal_flip=True, fill_mode="nearest")
         self.training_data = self.train_data_generator.flow_from_directory(self.train_data_dir,
-                target_size=(self.image_height, self.image_width), batch_size=self.batch_size, 
+                target_size=(64,64), batch_size=self.batch_size, 
                 class_mode="binary", classes=self.classes, color_mode="rgb")
         self.testing_data = self.test_data_generator.flow_from_directory(self.test_data_dir, 
-                target_size=(self.image_height, self.image_width), batch_size=self.batch_size,
+                target_size=(64,64), batch_size=self.batch_size,
                 class_mode="binary", classes=self.classes, color_mode="rgb")
+        """
+        self.train_data_generator = keras.preprocessing.image.ImageDataGenerator(samplewise_center=True,
+                samplewise_std_normalization=True,validation_split=0.1)
+        self.training_data = self.train_data_generator.flow_from_directory(self.train_data_dir,
+                target_size=(64,64), batch_size=64, shuffle=True,
+                subset="training")
+        self.testing_data = self.train_data_generator.flow_from_directory(self.test_data_dir,
+                target_size=(64,64), batch_size=64, subset="validation")
 
+    
+    def get_images(self):
+       print("Hello world!")         
+
+        
 class ConvNet:
     """ The convolutional neural net model """
     def __init__(self, t_dataset, t_first_activation="relu", t_height=200, t_width=200):
@@ -58,24 +76,48 @@ class ConvNet:
     def init_layers(self, t_first_activation):
         """ FIRST layer HAS to know about the input shape (in this case, should be (64, 200,200,3)
          (64,200,200,3) - batch_size=64, height and width = 200, and 3 RGB values """
-        self.model.add(keras.layers.Dense(self.data.batch_size, activation=t_first_activation, 
-                input_shape=(self.image_height, self.image_width, 3)))
+
+        # First layer of the model
+        # 32 filters, of size 140x140, using relu activation with the given input size and 3 colors RGB
+        self.model.add(keras.layers.Conv2D(64, kernel_size=10, strides=1,
+            activation="relu", input_shape=(64,64,3)))
+    
+    def build(self):
+        """ Add final layers, compile, and train the model """
+
+        self.model.add(keras.layers.Conv2D(64, kernel_size=4, strides=2, activation='relu'))
+        self.model.add(keras.layers.Dropout(0.5))
+        self.model.add(keras.layers.Conv2D(128, kernel_size=4, strides=1, activation='relu'))
+        self.model.add(keras.layers.Conv2D(128, kernel_size=4, strides=2, activation='relu'))
+        self.model.add(keras.layers.Dropout(0.5))
+        self.model.add(keras.layers.Conv2D(256, kernel_size=4, strides=1, activation='relu'))
+        self.model.add(keras.layers.Conv2D(256, kernel_size=4, strides=2, activation='relu'))
+        self.model.add(keras.layers.Flatten())
+        self.model.add(keras.layers.Dropout(0.5))
+        self.model.add(keras.layers.Dense(512, activation='relu'))
+        self.model.add(keras.layers.Dense(29, activation='softmax'))
+
+        print("CURRENT MODEL: ")
+        print(self.model.summary())
+    
+    def generate_previews(self):
+        img = keras.preprocessing.image.load_img("data/train/A/A2062.jpg")
+        x = keras.preprocessing.image.img_to_array(img)
+        x = x.reshape((1,) + x.shape)
+        i = 0
+        for batch in self.data.train_data_generator.flow(x, batch_size=1, save_to_dir="preview", save_prefix="A", save_format="jpeg"):
+            i += 1
+            if i > 20:
+                break
     
     def train(self):
-        """ Add final layers, compile, and train the model """
-        #self.model.add(keras.layers.Dense(10, activation="softmax"))
-        self.model.add(keras.layers.Conv2D(64, kernel_size=10, activation="relu",
-                input_shape=(self.data.image_height, self.data.image_width, 3)))
-        self.model.add(keras.layers.Conv2D(32, kernel_size=10, activation="relu"))
-        self.model.add(keras.layers.Flatten())
-        self.model.add(keras.layers.Dense(1, activation="softmax"))
 
         # Compilation process
-        self.model.compile(optimizer="rmsprop", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+        self.model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
 
         # Training process
-        self.model.fit_generator(self.data.training_data, steps_per_epoch=2000, epochs=50,
-                validation_data=self.data.testing_data, validation_steps=800)
+        self.model.fit_generator(self.data.training_data, epochs=5,
+                validation_data=self.data.testing_data)
     
     def predict(self):
         self.model.predict_generator(self.data.testing_data, use_multiprocessing=True, verbose=1)
@@ -83,8 +125,10 @@ class ConvNet:
 def main():
     data = DataSet("data/train", "data/test", t_batch_size=64)
     cnn = ConvNet(data)
+    cnn.build()
+    #cnn.generate_previews()
     cnn.train()
-    #cnn.model.fit_generator(cnn.data.train_data_generator, steps_per_epoch=2000, epochs=50, validation_data=cnn.data.test_data_generator, validation_steps=800)
+
     print("Done!")
 
 main()
